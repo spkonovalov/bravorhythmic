b9569b3dc016c27f544fe7147f99f725e275a89f
67path <sk@67path.com>
2026-09-24 12:38:48 -0700

    Analytics: Add Google Ads conversion tracking tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Roboto, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const roboto = Roboto({
@@ -28,6 +29,15 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${roboto.variable} ${geistMono.variable} h-full antialiased`}
     >
       <GoogleTagManager gtmId="GTM-5HX8JPHD" />
+      <Script src="https://www.googletagmanager.com/gtag/js?id=AW-18464232336" strategy="afterInteractive" />
+      <Script id="google-ads" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-18464232336');
+        `}
+      </Script>
       <body className="min-h-full flex flex-col">
         <Header />
         {children}
