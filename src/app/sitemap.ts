ae63cb6d002d6744bc4524a802f8cb0a5e48281b
67path <sk@67path.com>
2026-09-24 08:22:42 -0700

    SEO: Remove commute calculator from sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -17,12 +17,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     {
-      url: `${baseUrl}/commute-calculator`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
       url: `${baseUrl}/trial`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
